9b96dc1939b8122f39e249777c66e3926faaac07
Thanderobot <wanex17081994@gmail.com>
2025-03-07 16:40:53 +0300

    шаблон калькулятор

--- test_calculator.py
+++ test_calculator.py
@@ -3,3 +3,6 @@ from calculator import Calculator
 calc = Calculator()  # Создаем объект калькулятора
 calc.add(10, 10)       # Складываем 10 и 5
 calc.print_last_res() # Печатаем последний результат
+
+
+ тз
