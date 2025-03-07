4935e459a152a26d310c6f83a919f3ba5ee81de6
Thanderobot <wanex17081994@gmail.com>
2025-03-07 16:57:18 +0300

    task 1 реализованно умножение

--- calculator.py
+++ calculator.py
@@ -2,6 +2,11 @@ class Calculator:
     def __init__(self):
         self.last_result = None  # Переменная для хранения последнего результата
 
+    def multiply(self, a: float, b: float) -> float:
+        """Умножение двух чисел."""
+        self.last_result = a * b
+        return self.last_result
+
     def add(self, a, b):
         """Сложение двух чисел."""
         self.last_result = a + b
